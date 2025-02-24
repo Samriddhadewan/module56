210b51abc66dfe54a317aef80b91908450081fb4
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-02-24 16:52:31 +0600

    mongoDB connected

--- coffee-store-client/src/main.jsx
+++ coffee-store-client/src/main.jsx
@@ -3,8 +3,32 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AddCoffee from './Components/AddCoffee.jsx';
+import UpdateCoffee from './Components/UpdateCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/addCoffee",
+    element: <AddCoffee ></AddCoffee>
+    
+  },
+  {
+    path: "/updateCoffee",
+    element: <UpdateCoffee ></UpdateCoffee>
+  }
+]);
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
