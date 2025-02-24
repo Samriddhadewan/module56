7ccc13c89c9b7f01cb23660b3af64acdb27593d8
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-02-24 19:41:50 +0600

    video 5 done

--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -16,7 +16,6 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mx4ls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,12 +30,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeDB = client.db("coffeeDB").collection("coffees"); 
+
+    app.post("/coffees", async(req, res)=> {
+        const newCoffee = req.body;
+        console.log(newCoffee);
+        const result = await coffeeDB.insertOne(newCoffee);
+        res.send(result) 
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
